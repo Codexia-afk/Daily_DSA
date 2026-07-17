73f59e630661ececa6605eecabef414480f78afc
Srinjoy Pramanick <srinjoypramanick15@gmail.com>
2026-07-17 08:26:08 +0530

    Time: 26 ms (96.88%), Space: 102.5 MB (40.63%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int maximum = 0;
+
+        for (int num : nums) {
+            if (num > maximum) {
+                maximum = num;
+            }
+        }
+
+        long[] frequency = new long[maximum + 1];
+
+        for (int num : nums) {
+            frequency[num]++;
+        }
+
+        // exactGcd[g] = number of pairs whose GCD is exactly g
+        long[] exactGcd = new long[maximum + 1];
+
+        for (int g = maximum; g >= 1; g--) {
+            long divisibleCount = 0;
+
+            // Count numbers divisible by g
+            for (int multiple = g;
+                 multiple <= maximum;
+                 multiple += g) {
+                divisibleCount += frequency[multiple];
+            }
+
+            // Number of pairs where both numbers are divisible by g
+            long pairCount =
+                divisibleCount * (divisibleCount - 1) / 2;
+
+            // Remove pairs whose exact GCD is a larger multiple of g
+            for (int multiple = g * 2;
+                 multiple <= maximum;
+                 multiple += g) {
+                pairCount -= exactGcd[multiple];
+            }
+
+            exactGcd[g] = pairCount;
+        }
+
+        // prefixPairs[g] = number of pair GCD values <= g
+        long[] prefixPairs = new long[maximum + 1];
+
+        for (int g = 1; g <= maximum; g++) {
+            prefixPairs[g] =
+                prefixPairs[g - 1] + exactGcd[g];
+        }
+
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            long query = queries[i];
+
+            int left = 1;
+            int right = maximum;
+
+            while (left < right) {
+                int middle = left + (right - left) / 2;
+
+                if (prefixPairs[middle] > query) {
+                    right = middle;
+                } else {
+                    left = middle + 1;
+                }
+            }
+
+            answer[i] = left;
+        }
+
+        return answer;
+    }
+}
