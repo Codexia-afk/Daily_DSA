e007688e0bade6932dd7672d61c94104342e9942
Srinjoy Pramanick <srinjoypramanick15@gmail.com>
2026-09-07 08:19:18 +0530

    Time: 11 ms (35.48%), Space: 12.4 MB (48.39%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def distinctSubseqII(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        ends_with = [0] * 26
+
+        for ch in s:
+            idx = ord(ch) - ord("a")
+            ends_with[idx] = (sum(ends_with) + 1) % MOD
+
+        return sum(ends_with) % MOD
+        
